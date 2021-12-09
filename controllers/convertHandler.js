f381539dcd0d7fdfdf010132481b43bc26ed05fb
Quỳnh Vương <43884073+quynhvng@users.noreply.github.com>
2021-12-09 14:16:24 +0000

    Update convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,83 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    return input.match(/^\d+(\.\d+)?(\/\d+(\.\d+)?)?(?=[A-Za-z])/);
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    return input.match(/(?<=\d)[a-z]+$/i).toLowerCase();
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    const units = {
+      'gal': 'l',
+      'l': 'gal',
+      'lbs': 'kg',
+      'kg': 'lbs',
+      'mi': 'km',
+      'km': 'mi'
+    }
+    if (!Object.keys(units).includes(initUnit.toLowerCase())) {
+      console.log('Invalid unit');
+      return;
+    } else return units[initUnit];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    const fullUnit = {
+      'gal': 'gallon',
+      'l': 'liter',
+      'lbs': 'pound',
+      'kg': 'kilogram',
+      'mi': 'mile',
+      'km': 'kilometer'
+    }
+    if (!Object.keys(units).includes(unit.toLowerCase())) {
+      console.log('Invalid unit');
+      return;
+    } else return fullUnit[unit];
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    let returnNum = initNum;
+    switch (initUnit.toLowerCase()) {
+      case 'gal':
+        returnNum *= galToL;
+        break;
+      case 'l':
+        returnNum /= galToL;
+        break;
+      case 'lbs':
+        returnNum *= lbsToKg;
+        break;
+      case 'kg':
+        returnNum /= lbsToKg;
+        break;
+      case 'mi':
+        returnNum *= miToKm;
+        break;
+      case 'km':
+        returnNum /= miToKm;
+        break;
+      default:
+        console.log('Invalid unit');
+        return;
+    }
+    return {
+      returnNum: returnNum,
+      returnUnit: this.getReturnUnit(initUnit);
+    };
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    initUnit = this.spellOutUnit(initUnit.toLowerCase());
+    returnUnit = this.spellOutUnit(returnUnit.toLowerCase());
+    initUnit += (initNum == 1) ? '' : 's';
+    returnUnit += (returnNum == 1) ? '' : 's';
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
   
 }
