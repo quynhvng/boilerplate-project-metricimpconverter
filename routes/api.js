19be6134dcb52365793a087f4cff98b2315fb259
Quỳnh Vương <43884073+quynhvng@users.noreply.github.com>
2021-12-09 14:39:26 +0000

    Update api.js and .env

--- routes/api.js
+++ routes/api.js
@@ -6,5 +6,24 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
-
+  
+  app
+    .route('/api/convert')
+    .post(function (req, res) {
+      const inputStr = req.query.input;
+      const initNum = convertHandler.getNum(inputStr);
+      const initUnit = convertHandler.getUnit(inputStr);
+      const returnStr = convertHandler.convert(initNum, initUnit);
+      const returnNum = returnStr.returnNum;
+      const returnUnit = returnStr.returnUnit;
+      const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      const result = {
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+      };
+      res.json(result);
+    })
 };
